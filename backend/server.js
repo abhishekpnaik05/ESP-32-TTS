f6a5598c3f24603a41a1800c8ad8e670dc918626
abhishekpnaik05 <abhishekpnaik05@gmail.com>
2025-11-16 08:17:36 +0530

    bin update

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ let latestFirmwarePath = null;
 
 // ----- STATIC FRONTEND -----
 const __filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
+const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "./frontend")));
 
 // ----- HTTP SERVER -----
